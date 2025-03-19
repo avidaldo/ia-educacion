eb039c380c76499da051662b39b5f468c878a3d0
Alejandro Vidal <avd.formacion@gmail.com>
2025-03-19 20:14:21 +0100

    update docusaurus.config.ts, favicon.ico and logo.svg

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Docencia con IA',
+  tagline: 'Usando IAs para la docencia',
   favicon: 'img/favicon.ico',
 
   url: 'https://avidaldo.github.io',
