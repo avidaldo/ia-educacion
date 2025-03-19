43e938c929dea22732209100ad28f170e00c1362
Alejandro Vidal <avd.formacion@gmail.com>
2025-03-19 20:08:32 +0100

    delete 1 file and update 1 file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/1_historia_y_llms">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
