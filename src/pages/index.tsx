40b5bb07d459321b107fc71a0a5d4092255ec5e3
Alejandro Vidal <avd.formacion@gmail.com>
2025-03-20 17:50:37 +0100

    delete 17 files and update 1 file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/historia_y_llms">
-            Historia de la IA y LLMs
+            to="docs/herramientas">
+            Empezamos
           </Link>
         </div>
       </div>
